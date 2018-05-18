bad5e4bdb7d7f20e0f6534715f239579cffc1a7c
Saika <381207510@qq.com>
2018-05-18 16:15:09 +0800

    test ws

--- app/src/main/java/com/example/arapplication/MainActivity.java
+++ app/src/main/java/com/example/arapplication/MainActivity.java
@@ -12,7 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//git test
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
